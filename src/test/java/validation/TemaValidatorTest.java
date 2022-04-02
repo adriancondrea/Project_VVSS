0fc7c17f11e01b164584c668ad89c54f18220fb6
Condrea Adrian <adriancondrea2000@gmail.com>
2022-04-02 13:44:34 +0300

    Add test case

--- src/test/java/validation/TemaValidatorTest.java
+++ src/test/java/validation/TemaValidatorTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TemaValidatorTest {
     TemaValidator temaValidator = new TemaValidator();
+
     @BeforeEach
     void setUp() {
     }
@@ -18,8 +17,8 @@ class TemaValidatorTest {
     }
 
     @Test
-    void temaIdNull_throwsException(){
-        Tema tema = new Tema(null,"asa",10,1);
+    void temaIdNull_throwsException() {
+        Tema tema = new Tema(null, "description", 14, 1);
         try {
             this.temaValidator.validate(tema);
             assert false;
@@ -29,8 +28,8 @@ class TemaValidatorTest {
     }
 
     @Test
-    void temaIdEmpty_throwsException(){
-        Tema tema = new Tema("","asa",10,1);
+    void temaIdEmpty_throwsException() {
+        Tema tema = new Tema("", "description", 14, 1);
         try {
             this.temaValidator.validate(tema);
             assert false;
@@ -38,44 +37,103 @@ class TemaValidatorTest {
             assert true;
         }
     }
-    @Test
-    void temaIdValid_doesNotThrowException(){
-
-    }
-
 
     @Test
-    void temaDescriereNull_throwsException(){
-
-    }
-
-    @Test
-    void temaDescriereEmpty_throwsException(){
-
-    }
-    @Test
-    void temaDescriereValid_doesNotThrowException(){
-
-    }
-
-
-    @Test
-    void temaDeadlineLessThanLowerBound_throwsException(){
-
+    void temaDescriereNull_throwsException() {
+        Tema tema = new Tema("1", null, 14, 1);
+        try {
+            this.temaValidator.validate(tema);
+            assert false;
+        } catch (Exception e) {
+            assert true;
+        }
     }
 
     @Test
-    void temaDeadlineGreaterThanUpperBound_throwsException(){
-
+    void temaDescriereEmpty_throwsException() {
+        Tema tema = new Tema("1", "", 14, 1);
+        try {
+            this.temaValidator.validate(tema);
+            assert false;
+        } catch (Exception e) {
+            assert true;
+        }
     }
 
     @Test
-    void temaDeadlineSmallerThanStartline_throwsException(){
-
+    void temaDeadlineLessThanLowerBound_throwsException() {
+        Tema tema = new Tema("1", "description", 0, 1);
+        try {
+            this.temaValidator.validate(tema);
+            assert false;
+        } catch (Exception e) {
+            assert true;
+        }
     }
 
     @Test
-    void temaDeadlineValid_doesNotThrowException(){
+    void temaDeadlineGreaterThanUpperBound_throwsException() {
+        Tema tema = new Tema("1", "description", 15, 1);
+        try {
+            this.temaValidator.validate(tema);
+            assert false;
+        } catch (Exception e) {
+            assert true;
+        }
+    }
 
+    @Test
+    void temaDeadlineSmallerThanStartline_throwsException() {
+        Tema tema = new Tema("1", "description", 1, 3);
+        try {
+            this.temaValidator.validate(tema);
+            assert false;
+        } catch (Exception e) {
+            assert true;
+        }
+    }
+
+    @Test
+    void temaStartlineLessThanLowerBound_throwsException() {
+        Tema tema = new Tema("1", "description", 14, 0);
+        try {
+            this.temaValidator.validate(tema);
+            assert false;
+        } catch (Exception e) {
+            assert true;
+        }
+    }
+
+    @Test
+    void temaStartlineGreaterThanUpperBound_throwsException() {
+        Tema tema = new Tema("1", "description", 14, 15);
+        try {
+            this.temaValidator.validate(tema);
+            assert false;
+        } catch (Exception e) {
+            assert true;
+        }
+    }
+
+    @Test
+    void temaStartlineGraterThanDeadline_throwsException() {
+        Tema tema = new Tema("1", "description", 7, 10);
+        try {
+            this.temaValidator.validate(tema);
+            assert false;
+        } catch (Exception e) {
+            assert true;
+        }
+    }
+
+    @Test
+    void temaValid_throwsNoException() {
+        Tema tema = new Tema("1", "description", 14, 1);
+        try {
+            this.temaValidator.validate(tema);
+            assert true;
+        } catch (Exception e) {
+            assert false;
+        }
     }
 }
